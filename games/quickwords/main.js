8219f1120c226ee35f71d5a751eff41ebec1f229
Felix Sjöberg <felixsjoberg95@gmail.com>
2023-02-26 16:19:49 +0100

    game working

--- games/quickwords/main.js
+++ games/quickwords/main.js
@@ -1,76 +1,106 @@
+window.addEventListener('load', gameInitialize);
 'use strict';
 
+//variables
+let isPlaying;
+let time = 5;
+let activeGame = false;
+let score = 0;
+let words = []; // declare array to save words from text file.
+let timerInterval; // declare variable to save interval function.
+ 
+//DOM
 const gameBoard = document.getElementById('gameboard');
 const inputVal = gameBoard.querySelector('#input-word');
 let inputTimer = gameBoard.querySelector('#time');
 let word = gameBoard.querySelector('#word');
-
-// declare array to save words
-let words = [];
-
-//fetch words from text file
-fetch('words.txt')
-    .then(response => response.text())
-    .then(data => {
-        words = data.split('\n');
-    })
-    .catch(error => console.error(error));
+let currentScore = gameBoard.querySelector('#score');
 
 
-inputVal.addEventListener('keydown', startGame);
+//  Initialize game and fetch words from text file,
+// by doing like this I can use the words in the game,
+// and get a random word populated on the site directly.
+function gameInitialize() {
+    console.log('Game initialized');
+
+    fetch('../words.txt')
+        .then(response => response.text())
+        .then(data => {
+            words = data.split('\n');
+            randomWord(words);
+        })
+        .catch(error => console.error(error));
+}
 
 //   Set difficulty to value in local storage or medium by default
 function setDifficulty(d) {
+
     inputTimer.innerHTML = d;
+    time = d;
 }
 
-// function setActive() {
-//     let navLink = gameBoard.querySelector('.nav-link')
-//     navLink.addEventListener('click', () =>{
-//         navLink.classList.add('active')
-//     })
-// }
-        
+inputVal.addEventListener('keyup', startGame);
 
 function startGame() {
-    console.log('Game started');
+    if (checkWord()) {
+        clearInterval(timerInterval);
+        timer();
 
-    const word = randomWord()
-
-    checkWord(word);
-    timer(inputTimer.innerHTML); // Set timer to value in difficulty
-
+        isPlaying = true;
+        randomWord(words);
+        inputVal.value = '';
+        score++;
+        currentScore.innerHTML = score;
+    }
 }
 
-function timer(seconds) {
-    let i = 5;
-    i = seconds - 1;
-    if (i < 0) {
-        return;
-    }
-    let timer = setInterval(function () {
-        inputTimer.innerHTML = i;
-        i--;
-        if (i < 0) {
-            clearInterval(timer);
+
+
+function timer() {
+    let i = inputTimer.innerHTML;
+    timerInterval = setInterval(function () {
+        if (i <= 0) {
+            clearInterval(timerInterval);
+            isPlaying = false;
+            gameRestartHint();
+        }
+        else {
+            i--;
+            inputTimer.innerHTML = i;
         }
     }, 1000); // Delay between each iteration in milliseconds (1 second)
 }
 
-function checkWord(inputVal) {
-
-    const validWord = words.includes(word => word === inputVal.value)
-    if (validWord) {
-        console.log('Word is valid');
+// Named function expression, fixed problem with restarting game.
+let checkWord = function checkWord() {
+    if (inputVal.value === word.innerHTML) {
+        inputTimer.innerHTML = time;
+        return true;
+    } else {
+        return false;
     }
+};
+
+function gameRestartHint() {
+    word.classList.add('text-danger');
+    word.innerHTML = 'Game Over!';
+
+    gameBoard.querySelector('#hint').innerText = 'Press ENTER to play again';
+    
+    inputVal.addEventListener('keydown', function (e) {
+        if (e.key === 'Enter') {
+            score = 0;
+            currentScore.innerHTML = score;
+            setDifficulty(time);
+            randomWord(words);
+            startGame();
+        }
+    });
 }
-function randomWord() {
-    //get random number between 0 and 3871 which is the array length of words.
-    const randomNumber = Math.floor(Math.random() * Math.floor(3871));
 
-    const randomWord = words[randomNumber];
-    word.innerText = randomWord;
+function randomWord(words) {
+    const randomNumber = Math.floor(Math.random() * words.length);
+    word.innerHTML = words[randomNumber];
     word.classList.add('text-info', 'text-center', 'display-4');
-
-    return randomWord;
+    word.classList.remove('text-danger');
 }
